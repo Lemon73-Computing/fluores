a675cdfb3f5d5e104651af57c0dff6b208847330
Lemon73-Computing <Lemonade.flash.1164@gmail.com>
2025-01-25 22:02:10 +0900

    feat(vuetify): set default theme based on user's color scheme preference

--- src/main.ts
+++ src/main.ts
@@ -12,6 +12,9 @@ import App from './App.vue'
 const vuetify = createVuetify({
   components,
   directives,
+  theme: {
+    defaultTheme: window.matchMedia('(prefers-color-scheme: dark)').matches ? 'dark' : 'light',
+  },
 })
 
 createApp(App).use(vuetify).mount('#app')
